cda9f63cbbd8e58ccbe1cd56e06f0f2eca4fecc4
Stanley Chen <dannyshien@gmail.com>
2021-02-02 17:55:27 -0500

    Set up initial structure for BodyNavbar

--- src/containers/about/About.js
+++ src/containers/about/About.js
@@ -5,20 +5,12 @@ class About extends Component {
   render() {
     return (
       <>
-        <section className="body__navbar">
-          <Link to="#" className="navbar__title">
-            Rev. Ki Kim's Bio
-          </Link>
-          <Link to="#" className="navbar__title">
-            Mission Statement
-          </Link>
-          <Link to="#" className="navbar__title">
-            Vision
-          </Link>
-          <Link to="#" className="navbar__title">
-            Contact
-          </Link>
-        </section>
+        <BodyNavbar
+          kim="Rev. Ki Kim's Bio"
+          statement="Mission Statement"
+          vision="Vision"
+          contact="Contacts"
+        />
 
         <section className="about__container">
           <div className="nav__title">
