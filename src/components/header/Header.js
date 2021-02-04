1a1519b232794a5fadf133f7abd3b2ec242b20dc
Stanley Chen <dannyshien@gmail.com>
2021-02-04 18:31:35 -0500

    Install NPM and react-router-dom, adjust size for logo image

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../navbar/Navbar";
+// import Logo from "../../assets/CCPC_LOGO_FINAL3.jpg";
+import Logo from "../../assets/ccpc_logo.jpg";
 
 const Header = () => {
   return (
     <div className="header">
-      <img className="logo__image" />
+      <img className="logo__image" src={Logo} alt="church logo" />
       <h1 className="main__title"></h1>
       <Navbar sermons="Previous Sermons" live="Live Service" about="About" />
     </div>
