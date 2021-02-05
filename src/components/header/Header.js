a43d7b5646931880bf6cb997360e3861982eb6a9
Stanley Chen <dannyshien@gmail.com>
2021-02-04 19:09:41 -0500

    Move css preset from App to index.css

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./Header.css";
+
 import Navbar from "../navbar/Navbar";
 // import Logo from "../../assets/CCPC_LOGO_FINAL3.jpg";
 import Logo from "../../assets/ccpc_logo.jpg";
@@ -7,7 +9,7 @@ const Header = () => {
   return (
     <div className="header">
       <img className="logo__image" src={Logo} alt="church logo" />
-      <h1 className="main__title"></h1>
+      <h1 className="main__title">Crossings Community Presbyterian Church</h1>
       <Navbar sermons="Previous Sermons" live="Live Service" about="About" />
     </div>
   );
