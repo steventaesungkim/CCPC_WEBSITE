bdcfc835fd490562acbc8e60b87827ffb563674b
Steven Taesung Kim <steventaesungkim@gmail.com>
2021-02-11 22:30:20 -0500

    added paths on bodyNavbar

--- src/components/navbar/BodyNavbar.js
+++ src/components/navbar/BodyNavbar.js
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 const BodyNavbar = ({ kim, statement, vision, contact }) => {
   return (
     <section className="body__navbar">
-      <Link to="#" className="navbar__title">
+      <Link to="/about/bio" className="navbar__title">
         {kim}
       </Link>
-      <Link to="#" className="navbar__title">
+      <Link to="/about/statement" className="navbar__title">
         {statement}
       </Link>
-      <Link to="#" className="navbar__title">
+      <Link to="/about/vision" className="navbar__title">
         {vision}
       </Link>
-      <Link to="#" className="navbar__title">
+      <Link to="/about/contact" className="navbar__title">
         {contact}
       </Link>
     </section>
