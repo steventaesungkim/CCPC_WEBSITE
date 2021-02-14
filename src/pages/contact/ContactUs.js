385b871e70586a9b1c7ac92f2b26b997f1008e29
Stanley Chen <dannyshien@gmail.com>
2021-02-14 14:14:04 -0500

    Add css file for contact folder, import global contactus css files

--- src/pages/contact/ContactUs.js
+++ src/pages/contact/ContactUs.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
+import "../../stylesheet/Styles.css";
+import "../contact/ContactUs.css";
+import Divider from "../../assets/Divider.png";
 
 class Contact extends Component {
   state = {};
 
   render() {
     return (
-      <div className="contact__container">
-        <h2>Contact Us</h2>
-        <div className-="form__body">
+      <div className="container">
+        <h2 className="sub__title">Contact Us</h2>
+        <img className="divider" src={Divider} alt="" />
+        <div className="form__body content">
           <p>Helloooooo, call me please!</p>
           {/* <form></form> */}
         </div>
